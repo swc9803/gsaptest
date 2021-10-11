7980a38861b4163c7b05493e8608f835eab2d782
swc9803 <swc9803@gmail.com>
2021-10-11 20:27:02 +0900

    기본 세팅

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  Home
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  Test
+  git add test
+</template>
+
+<script>
+import { gsap } from 'gsap'
+import { onMounted } from 'vue'
+
+export default {
+  setup () {
+    onMounted(() => {
+      gsap.to('div', { xPercent: 20 })
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
